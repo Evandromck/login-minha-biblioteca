8fb60e7438a66d5a546843bba603024cc8aa8d3d
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-01-28 17:35:20 -0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Imgcapa from './assets/capa.png'
+import Imglivra from './assets/livraria.png'
+import Imgport from './assets/portariaemerj.png'
+import Imglogo from './assets/logo.png'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-capa">
+        <span className="login-form-capa">
+          <img src={Imgcapa} alt="capa-front" />
+          <img src={Imglivra} alt="livra-front" />
+          <img src={Imgport} alt="port-front" />
+          <img src={Imglogo} alt="logo-front" />
+          <span className="login-form-title">Login</span>
+          
+          <div className="wrap-input">
+            <input type="email" />
+            <span className="focus-input" data-placeholder="Email"></span>
+          </div>
+
+          <div className="wrap-input">
+            <input type="password" />
+            <span className="focus-input" data-placeholder="Password"></span>
+          </div>
+
+          <div className="container-login-form-btn">
+            <button className="login-form-btn">Login</button>
+          </div>
+
+         <div className="text-center">
+            <a className="txt1" href="https://www.google.com.br">Recuperar senha</a>
+         </div>
+          
+        </span>
+        
+      </div>
+
     </div>
   );
 }
