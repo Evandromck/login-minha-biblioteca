c1f8e0a66db9768d281e5159726c6030af253b7c
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-02-08 15:36:26 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-//import About from './screens/About';
-//import App from './screens/App';
-//import Contact from './screens/Contact';
-//import Courses from './screens/Courses';
-//import CoursesSchedules from './screens/CoursesSchedules';
 import Home from "./screens/Home";
 import Login from "./screens/Login";
-//import Users from './screens/Users';
-//import UsersNew from './screens/UsersNew';
 
 ReactDOM.render(
   <BrowserRouter>
